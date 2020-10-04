3069669f3b9c29efdfcf48f3825fc7fce49ecd2d
Anita Dürr <anita.d.durr@gmail.com>
2020-10-04 13:02:34 +0200

    create implementations.py, README.md and run.py files

---
+++ implementations.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def least_sqaures_GD(y, tx, initial_w, max_iters, gamma):
+	None
+
+def least_squares SGD(y, tx, initial w, max iters, gamma):
+	None
+
+def least_squares(y, tx):
+	None
+
+def ridge_regression(y, tx, lambda_):
+	None
+
+def logistic_regression(y, tx, initial w, max iters, gamma):
+	None
+
+def reg_logistic_regression(y, tx, lambda_, initial w, max iters, gamma):
+	None
+
+
