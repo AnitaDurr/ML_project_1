f47e7ceef044a8a95797cc2e33a14cb39682613e
Anita Dürr <anita.d.durr@gmail.com>
2020-10-13 15:38:31 +0200

    add costs functions

--- implementations.py
+++ implementations.py
@@ -1,9 +1,45 @@
 import numpy as np
 
-def least_sqaures_GD(y, tx, initial_w, max_iters, gamma):
-	None
+
+def compute_mse(y, tx, w):
+	"""
+	Compute the mse loss.
+	"""
+	e = y - tx.dot(w)
+	mse = e.dot(e) / (2 * len(e))
+	return mse
+
+def compute_rmse(y, tx, w):
+	"""
+	Compute the rmse loss.
+	"""
+	return np.sqrt(2 * compute_mse(y, tx, w))
+
+def compute_mae(y, tx, w):
+	"""
+	Compute the mae loss.
+	"""
+	e = y - tx.dot(w)
+    return np.mean(np.absolute(e))
+
+def mse_gradient(y, tx, w):
+	"""
+	Gradient of the linear mse function.
+	"""
+	e = y - tx.dot(w)
+	return - tx.T.dot(e) / y.shape[0]
+
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+	"""
+	Linear regression using gradient descent.
+	"""
+	return w, loss
 
 def least_squares SGD(y, tx, initial w, max iters, gamma):
+	"""
+	Linear regression using stochastic gradient descent, with mini-batch size 1.
+	"""
 	None
 
 def least_squares(y, tx):
