bfed05fbd7e2d079037267f1041591409ee9d0db
Mass23 <33321080+Mass23@users.noreply.github.com>
2020-10-19 08:54:33 +0200

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def load_train_data(file):
+    X = np.genfromtxt(file, delimiter=",", skip_header=1, usecols=[i for i in range(2,32)])
+    y = np.genfromtxt(file, delimiter=",", skip_header=1, usecols=[1], dtype = 'U25')
+    return(X, y)
