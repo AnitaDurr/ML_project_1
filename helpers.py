1dd577a2fdba0b6983bbc3643033bf0cb9a5fe04
Mass23 <33321080+Mass23@users.noreply.github.com>
2020-10-19 08:59:34 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -4,3 +4,7 @@ def load_train_data(file):
     X = np.genfromtxt(file, delimiter=",", skip_header=1, usecols=[i for i in range(2,32)])
     y = np.genfromtxt(file, delimiter=",", skip_header=1, usecols=[1], dtype = 'U25')
     return(X, y)
+
+def load_test_data(file):
+    X = np.genfromtxt(file, delimiter=",", skip_header=1, usecols=[i for i in range(2,32)])
+    return(X)
