45290a2083488cfc67550fb837e66715565d2804
Mass23 <33321080+Mass23@users.noreply.github.com>
2020-10-19 10:27:00 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def load_train_data(file):
     X = np.genfromtxt(file, delimiter=",", skip_header=1, usecols=[i for i in range(2,32)])
-    y = np.genfromtxt(file, delimiter=",", skip_header=1, usecols=[1], dtype = 'U25')
+    y = np.genfromtxt(file, delimiter=",", skip_header=1, usecols=[1], dtype = None, converters={1: lambda x: 0 if b's' in x else 1})
     return(X, y)
 
 def load_test_data(file):
