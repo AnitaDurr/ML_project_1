21588d708ae19862d6b28f68a92793a80bd6d252
Mass23 <33321080+Mass23@users.noreply.github.com>
2020-10-25 07:35:34 +0100

    Update tune_hyperparam.py

--- tune_hyperparam.py
+++ tune_hyperparam.py
@@ -47,6 +47,15 @@ def cv_ls_gd_sgd(hprange, loss_tr_gd, loss_te_gd, loss_tr_sgd, loss_te_sgd):
 	plt.grid(True)
 	plt.savefig("GD_SGD")
 
+def cv_log_reg(gamma_range, lambda_range, loss_df, filename, plot_title):
+	ax = plt.axes()
+	sns.heatmap(loss_df, ax = ax)
+	ax.set_title(plot_title)
+	ax.set_xticklabels([str(i) for i in gamma_range])
+	ax.set_yticklabels([str(i) for i in lambda_range])
+	plt.savefig(filename)
+	plt.close()
+
 ### SCRIPT
 
 # will be commented as this script will be called from run.py where this global variable is defined
@@ -99,10 +108,56 @@ rr_args = [best_RR_lambda]
 #plot
 cv_ls_rr(lambdas, loss_tr, loss_te)
 
-
-
-
 ### LOGISTIC REGRESSION
+# logistic == logistic regression
+# reg_log == regularized logistic regression
+k_fold = 5
+seed = 7
+lambdas = np.arange(0, 21, 20)
+gammas = np.logspace(-12, -5, num=20)
+w_initial = np.zeros(x.shape[1])
+max_iters = 100
+
+print("[Logistic regressions]", end=" ")
+
+t1 = time.time()
+
+# For this one we have to tune both gammas and lambdas.
+# We use one for both and compute the best gamma after the lambda_ == 0 iteration for simple logistic regression
+# Then continue the iterations for penalised logistic regression
+for lambda_ in lambdas:
+	if lambda_ == 0:
+		log_best_gamma, log_loss_tr, log_loss_te = tune_hyperparam(y, x, k_fold, seed, hprange=gammas, method=logistic_regression, args=[w_initial, max_iters], compute_loss=neg_log_likelihood)
+		# First iteration: creates numpy arrays that will become data frames for heatmaps
+		reg_loss_tr_df = log_loss_tr
+		reg_loss_te_df = log_loss_te
+		t2 = time.time()
+		print("time:", t2 - t1, "Logistic regression best gamma:", log_best_gamma)
+		# Start storing best values to keep the best one over lambda iterations
+		reg_best_gamma = 0
+		reg_best_lambda = 0
+		reg_best_loss = np.argmin(log_loss_te)
+	else:
+		reg_best_gamma, reg_loss_tr, reg_loss_te = tune_hyperparam(y, x, k_fold, seed, hprange=gammas, method=reg_logistic_regression, args=[w_initial, lambda_, max_iters], compute_loss=neg_log_likelihood)
+		# Stack this lambda result to the data frames
+		reg_loss_tr_df = np.vstack((reg_loss_tr_df,reg_loss_tr))
+		reg_loss_te_df = np.vstack((reg_loss_te_df,reg_loss_te))
+
+		if np.argmin(reg_loss_tr) < reg_best_loss:
+			reg_best_loss = np.argmin(reg_loss_tr)
+			reg_best_lambda = lambda_
+			reg_best_gamma = gamma
+t3 = time.time()
+# find best gamma in reg
+print("time:", t3 - t1, "Regularized logistic regression best gamma:", reg_best_gamma)
+print(reg_loss_te_df)
+print(reg_loss_tr_df)
+
+cv_log_reg(gammas, lambdas, reg_loss_tr_df, 'Reg_log_train.pdf', '-log likelihood for reg. logistic regression: train data')
+cv_log_reg(gammas, lambdas, reg_loss_te_df, 'Reg_log_test.pdf', '-log likelihood for reg. logistic regression: test data')
+
+logistic_args = [w_initial, max_iters, log_best_gamma]
+reg_log_args = [w_initial, max_iters, reg_best_gamma]
 
 
 
