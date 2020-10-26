4f26f234812e3c8ee614bba89612ff292dd42e89
Mass23 <33321080+Mass23@users.noreply.github.com>
2020-10-26 13:15:40 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 
 from implementations import *
 from helpers import *
-from compare_method import *
+from compare_method import * 		# this will execute the compare_method script
 
 # best_method and best_args should have been selected in compare_method script
 
@@ -12,13 +12,14 @@ weights, loss = best_method(y, tx, *best_args)
 # Generate predictions and save ouput in csv format for submission
 print('===LOADING TEST DATA===')
 DATA_TEST_PATH = 'data/test.csv'
-_, tx_test, ids_test = load_clean_data(DATA_TEST_PATH)
+_, tx_test, ids_test = load_clean_data(DATA_TEST_PATH, feature_selection=False)
+tx_test = np.delete(tx_test, features_to_remove, axis=1)
 
-
+print('===PREDICTION===')
 OUTPUT_PATH = 'data/predict.csv'
-is_LR = False
 if best_method in [logistic_regression, reg_logistic_regression]:
-    y_pred = predict_labels(weights, tx, is_LR=True)
+    y_pred = predict_labels(weights, tx, is_LR=True, submission=True)
 else:
-    y_pred = predict_labels(weights, tx_test, is_LR=False)
+    y_pred = predict_labels(weights, tx_test, is_LR=False, submission=True)
 create_csv_submission(ids_test, y_pred, OUTPUT_PATH)
+print('===SUBMISSION FILE CREATED===')
