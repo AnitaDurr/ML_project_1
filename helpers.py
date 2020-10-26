73bcb542544464c7fe742a584accd008aa727667
Mass23 <33321080+Mass23@users.noreply.github.com>
2020-10-26 14:19:22 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -152,10 +152,10 @@ def predict_labels(w, tx, is_LR=False, submission=False):
     Generates class predictions given weights, and a test data matrix
     """
     # submission: put zeros and ones
-    lower = -1
+    lower = 0
     upper = 1
     if submission == True:
-        lower = 0
+        lower = -1
 
     if is_LR == True:
         predictions = []
