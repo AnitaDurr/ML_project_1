f4fa662c00940128596896e4bca299210b6a126a
Mass23 <33321080+Mass23@users.noreply.github.com>
2020-10-26 12:27:04 +0100

    Update run.py

--- run.py
+++ run.py
@@ -5,17 +5,26 @@ from implementations import *
 from helpers import *
 
 # load training data into class labels, feature matrix and event ids
-# DATA_TRAIN_PATH = 'data/train.csv'
-# print('===LOADING DATA===')
-# y, tx, ids = load_clean_data(DATA_TRAIN_PATH)
+DATA_TRAIN_PATH = 'data/train.csv'
+print('===LOADING TRAIN DATA===')
+y, tx, ids = load_clean_data(DATA_TRAIN_PATH)
 
 from compare_method import * 		# this will execute the compare_method script
 
-weights = best_method(y, tx, best_args)
+# best_method and best_args should have been selected in compare_method script
+
+weights, loss = best_method(y, tx, *best_args)
 
 # Generate predictions and save ouput in csv format for submission
+print('===LOADING TEST DATA===')
 DATA_TEST_PATH = 'data/test.csv'
-OUTPUT_PATH = 'data/predict.csv'
 _, tx_test, ids_test = load_clean_data(DATA_TEST_PATH)
-y_pred = predict_labels(weights, tx_test)
+
+
+OUTPUT_PATH = 'data/predict.csv'
+is_LR = False
+if best_method in [logistic_regression, reg_logistic_regression]:
+    y_pred = predict_labels(weights, tx, is_LR=True)
+else:
+    y_pred = predict_labels(weights, tx_test, is_LR=False)
 create_csv_submission(ids_test, y_pred, OUTPUT_PATH)
