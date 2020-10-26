747247715b44d6064304e946c56861edd2abebf8
Mass23 <33321080+Mass23@users.noreply.github.com>
2020-10-26 08:03:03 +0100

    Update cross_validation.py

--- cross_validation.py
+++ cross_validation.py
@@ -47,12 +47,12 @@ def cv_loss(y, x, k_indices, method, args, compute_loss):
 
 		# learn the weights and compute the train and test loss
 		weights, k_loss_tr = method(y_tr, x_tr, *args)
-		
-		if method == reg_logistic_regression:
-			# For negative log-likelihood: normalise by sample size to compare train and test
-			k_loss_tr = k_loss_tr / y_tr.shape[0]
+
+		if method in [logistic_regression, reg_logistic_regression]:
+			# For negative log-likelihood: normalise by sample size to compare train and test losses
+			k_loss_tr = k_loss_tr / len(y_tr)
 			k_loss_te = neg_log_likelihood(y_te, x_te, weights)
-			k_loss_te = k_loss_te / y_te.shape[0]
+			k_loss_te = k_loss_te / len(y_te)
 		else:
 			k_loss_te = compute_loss(y_te, x_te, weights)
 
@@ -81,7 +81,7 @@ def cv_criterions(y, x, k_indices, method, args, verbose=True):
 
 		# learn the weights and compute the test criterions
 		weights, _ = method(y_tr, x_tr, *args)
-		accuracy, precision, recall, f1_score = compute_criterions(y_te, x_te, weights)
+		accuracy, precision, recall, f1_score = compute_criterions(y_te, x_te, weights, method)
 
 		# store the test criterions
 		accuracy_te.append(accuracy)
@@ -90,10 +90,10 @@ def cv_criterions(y, x, k_indices, method, args, verbose=True):
 		f1_score_te.append(f1_score)
 
 	# average the k results
-	cv_accuracy = sum(accuracy_te) / k_fold
-	cv_precision = sum(precision_te) / k_fold
-	cv_recall = sum(recall_te) / k_fold
-	cv_f1_score = sum(f1_score_te) / k_fold
+	cv_accuracy = np.sum(accuracy_te) / k_fold
+	cv_precision = np.sum(precision_te) / k_fold
+	cv_recall = np.sum(recall_te) / k_fold
+	cv_f1_score = np.sum(f1_score_te) / k_fold
 
 	if verbose:
 		print(method.__name__)
@@ -102,5 +102,4 @@ def cv_criterions(y, x, k_indices, method, args, verbose=True):
 		print("  - recall={r}						".format(r=cv_recall))
 		print("  - F1 score={f}						".format(f=cv_f1_score))
 
-	return accuracy_te, precision_te, cv_recall_te, cv_f1_score_te
-
+	return accuracy_te, precision_te, recall_te, f1_score_te
